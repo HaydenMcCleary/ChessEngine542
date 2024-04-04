f7a4d5cfa5d0200b056da12c493799c11571ccb1
HaydenMcCleary <hayden0pm@gmail.com>
2024-04-04 19:47:50 -0400

    Working Data Extraction data

--- extractdata.py
+++ extractdata.py
@@ -16,9 +16,14 @@ with jsonlines.open(file_path, "r") as reader:
         # Extract evaluations and associated lines of moves
         high_score = 0  # Initialize with negative infinity to ensure the first score is always higher
         best_move = ""
+        is_mate = False  # Flag to indicate if mate is detected
         
         for eval_data in line["evals"]:
             for pv in eval_data["pvs"]:
+                # Check if mate is detected
+                if "mate" in pv:
+                    is_mate = True
+                    break  # Exit the loop if mate is detected
                 # Extract line of moves and evaluation score
                 line_moves = pv["line"]
                 eval_score = pv.get("cp")
@@ -27,9 +32,13 @@ with jsonlines.open(file_path, "r") as reader:
                 if eval_score is not None and abs(eval_score) > abs(high_score):
                     high_score = eval_score
                     best_move = line_moves
+            
+            if is_mate:
+                best_move = line_moves
+                break  # Exit the loop if mate is detected
         
         # Store the best move and its evaluation score in the dictionary
-        data_dict[fen_position] = (best_move, high_score)
+        data_dict[fen_position] = (best_move, "Mate" if is_mate else high_score)
 
 # Print to check
 for fen, (move, score) in data_dict.items():
